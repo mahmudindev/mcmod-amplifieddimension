7568abec44ea5b24205de6bcdd10493872a9fb3d
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-07-26 15:12:32 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/amplifieddimension/neoforge/AmplifiedDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.amplifieddimension.neoforge;
+
+import com.github.mahmudindev.mcmod.amplifieddimension.AmplifiedDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AmplifiedDimension.MOD_ID)
+public final class AmplifiedDimensionNeoForge {
+    public AmplifiedDimensionNeoForge() {
+        // Run our common setup.
+        AmplifiedDimension.init();
+    }
+}
